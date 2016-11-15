14e0d6e2bdc7c083950a0acd4e50408232edf3ae
Glanzer <glanzer.markus@gmail.com>
2016-11-15 17:19:03 +0100

    not much yet 	new file:   sftk.py

---
+++ sftk.py
@@ -0,0 +1,18 @@
+#! /bin/python
+'''
+    Savage.py toolkit, let's what it's good for.
+'''
+
+class planet():
+    def __init__(self):
+        self.name=None
+        self.gravity=None
+        self.terrain=None
+        self.atmosph=None
+        self.popolus=None
+        self.govd=None
+        self.law=None
+        self.spport=None
+        self.tech=None
+        self.coords=None
+        
